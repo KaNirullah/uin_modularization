a9e4f4cb51fc46a16bbf76ddf716b0cf19d1955f
KaNirullah <56153758+kanirullah@users.noreply.github.com>
2019-10-11 16:41:40 +0700

    Kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,12 @@
+nama = 'Muh Hamzah Tsalis N'
+program = 'Gerak Lurus'
+
+print(f'Program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print(f'jarak ={jarak/1000} ditempuh dalam waktu = {waktu/60}menit')
+print(f'Sehingga kecepatan = {kecepatan} m/s')
+
